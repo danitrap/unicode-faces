d4ce8caea8a5ac77ff6773f8fea67cf1e396c378
Daniele Trapani <daniele.trapani@live.it>
2014-10-01 12:55:56 +0200

    Add clipboard plugin, basic faces, styles and javascript

---
+++ www/js/index.js
@@ -0,0 +1,30 @@
+var App = {
+    initApplication: function () {
+        $('.face').click(App.onFaceTouch);
+    },
+    onFaceTouch: function (ev) {
+        ev.preventDefault();
+        console.log(this.innerHTML + " face touched");
+        App.copyToClipboard(this.innerHTML);
+        App.showCopied();
+    },
+    copyToClipboard: function (face) {
+        cordova.plugins.clipboard.copy(face);
+    },
+    showCopied: function () {
+        $.mobile.loading( "show", {
+            text: "Face copied to clipboard.",
+            textVisible: true,
+            theme: "b",
+            textonly: true,
+            html: ""
+        });
+
+        setTimeout( function () {
+            $.mobile.loading( "hide" );
+        }, 1000);
+    }
+}
+
+$(document).one('deviceready', App.initApplication);
+// $(document).ready(App.initApplication);
